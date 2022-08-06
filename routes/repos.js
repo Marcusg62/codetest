3122a1c861e9fd7458eb8aa0289e643168fcd286
Marcus Gallegos <marcus@developdenver.tech>
2022-08-06 15:39:48 -0600

    added to dos

--- routes/repos.js
+++ routes/repos.js
@@ -6,12 +6,12 @@ const axios = require('axios');
 router.get('/', async (req, res, next) => {
     const config = {
         method: 'get',
-        url: 'https://api.github.com/repos/twbs/bootstrap/pulls',
-        headers: {}
+        url: 'https://api.github.com/repos/twbs/bootstrap' + '/pulls', // TO DO: make dyanmic with query string 
+        headers: {} // TO DO: implement authenticated users PRs
     };
 
     let response = await axios(config);
-    res.send(response.data)
+    res.send(response.data) // TO DO: how to paginate? default length is 30
 });
 
 module.exports = router;
