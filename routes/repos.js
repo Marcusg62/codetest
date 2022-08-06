eff205ca2abb5074c63542bf763f7a96e6bb7566
Marcus Gallegos <marcus@developdenver.tech>
2022-08-06 15:28:47 -0600

    basic list of pull request

--- routes/repos.js
+++ routes/repos.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET a repo */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', async (req, res, next) => {
+    const config = {
+        method: 'get',
+        url: 'https://api.github.com/repos/twbs/bootstrap/pulls',
+        headers: {}
+    };
+
+    let response = await axios(config);
+    res.send(response.data)
 });
 
 module.exports = router;
