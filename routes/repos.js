6f4bece8a4b0350e6d02816e7b048f6447f275e9
Marcus Gallegos <marcus@developdenver.tech>
2022-08-07 17:14:43 -0600

    Implement test for checking response properties

--- routes/repos.js
+++ routes/repos.js
@@ -7,7 +7,7 @@ const { query, validationResult } = require('express-validator');
 router.get('/',
     query('owner').notEmpty(), // express-validator middleware
     query('repo').notEmpty(), // express-validator middleware
-    async (req, res) => {
+    async (req, res, next) => {
         try {
             // send error message if required query params are not present
             const errors = validationResult(req);
@@ -17,24 +17,27 @@ router.get('/',
 
             const config = {
                 method: 'get',
-                url: `https://api.github.com/repos/${owner}/${repo}/pulls`,
+                url: `https://api.github.com/repos/${req.query.owner}/${req.query.repo}/pulls`,
                 headers: {}
             };
             let response = await axios(config);
             response.data = response.data.map(pr => {
+                console.log('pr', pr)
+                
                 return {
                     id: pr.id,
                     number: pr.number,
                     title: pr.title,
-                    author: pr.author,
-                    commit_count: pr.commit_count
+                    author: pr.author, // to do: fetch details from user field
+                    commit_count: pr.commit_count // to do: fetch commit count _links .commits.href
                 };
             });
             res.send(response.data) // TO DO: how to paginate? default length is 30
         }
         catch (error) {
-            return res.status(500);
+            return res.sendStatus(500).send(error);
         }
+
     });
 
 module.exports = router;
